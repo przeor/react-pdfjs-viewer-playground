27b8a8630d86045ccbbe212ca6898eeb2e87023f
andrzejwodynski <andrzej.wodynski@skygate.pl>
2015-04-10 14:38:40 +0200

    actions test

--- react_app/src/js/actions/app-actions.js
+++ react_app/src/js/actions/app-actions.js
@@ -3,11 +3,20 @@ var AppConstants = require('../constants/app-constants.js');
 var AppDispatcher = require('../dispatchers/app-dispatcher.js');
 
 var AppActions = {
+
   addItem:function(){
     AppDispatcher.handleViewAction({
       actionType: AppConstants.ADD_ITEM
     })
   }
-}
+};
+
+console.log('startujemy',window);
+
+
+window.addItemTest = function () {
+    console.log('actionsAddItem');
+    AppActions.addItem();
+};
 
 module.exports = AppActions;
